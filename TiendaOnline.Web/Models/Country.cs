1c83d3ca16146117a6bb08587e6b25865c0e9293
SANTIAGO RUIZ LONDOÑO <santiagoruiz247667@correo.itm.edu.co>
2022-03-28 15:44:03 -0500

    Improve countries CRUD

---
+++ TiendaOnline.Web/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaOnline.Web.Models
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe contener almenos un caracter")]
+
+        public string Name { get; set; }
+
+    }
+}
